a3c0181e69985a6a14cab13e8b53e3178aa9450b
mmarolin <matija.marolin11@gmail.com>
2022-04-05 15:21:15 +0200

    updating-navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./routes/home/home.component";
-
-const Shop = () => {
-  return <h1>SHOP PAGE</h1>;
-};
-
-const Navbar = () => {
-  return (
-    <div>
-      <div>
-        <h1>NAVBAR</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from "./components/navigation/navigation.component";
+import Shop from "./components/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navbar />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,5 @@
+const Shop = () => {
+  return <h1>SHOP PAGE</h1>;
+};
+
+export default Shop;
